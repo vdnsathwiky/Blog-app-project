294ae4701847973aa7dedc70bf2517b13ecfd537
V D N SATHWIK YARLAGADDA <vdnsathwikyarlagadda@gmail.com>
2026-09-25 11:53:50 +0530

    Update app structure with split modules

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify
 from database import mongo
 from config import MONGO_URI
 from auth import auth_bp
-from blogs import blogs_bp
+from create_blog import create_blog_bp
+from blog_routes import blog_routes_bp
+from blog_actions import blog_actions_bp
+from likes import likes_bp
+from comments import comments_bp
 
 app = Flask(__name__)
 
@@ -11,7 +15,11 @@ app.config["MONGO_URI"] = MONGO_URI
 mongo.init_app(app)
 
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
-app.register_blueprint(blogs_bp, url_prefix="/api")
+app.register_blueprint(create_blog_bp, url_prefix="/api")
+app.register_blueprint(blog_routes_bp, url_prefix="/api")
+app.register_blueprint(blog_actions_bp, url_prefix="/api")
+app.register_blueprint(likes_bp, url_prefix="/api")
+app.register_blueprint(comments_bp, url_prefix="/api")
 
 
 @app.route("/")
